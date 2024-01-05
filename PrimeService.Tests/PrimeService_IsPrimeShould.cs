7f20341b0386f44dd9da8982dfb75799e585e758
Don Stierman <stiermandon@gmail.com>
2024-01-04 21:21:17 -0700

    cleanup code

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -3,15 +3,29 @@ using Prime.Services;
 
 namespace Prime.UnitTests.Services
 {
-    public class PrimeService_IsPrimeShould
+    public class PrimeService_IsEven
     {
         [Fact]
         public void IsPrime_InputIs1_ReturnFalse()
         {
             var primeService = new PrimeService();
+            int value = 1;
             bool result = primeService.IsPrime(1);
 
-            Assert.False(result, "1 should not be prime");
+            Assert.False(result, $"{value} is not equal to 1");
         }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void IsPrime_InputIsEven_ReturnFalse(int value)
+        {
+            var primeService = new PrimeService();
+            var result = primeService.IsEven(value);
+
+            Assert.False(result, $"{value} should not be even");
+        }
+
     }
 }
--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,14 +4,17 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-        /*
-        public bool IsPrime(int candidate)
-        {
-            throw new NotImplementedException("Not implemented.");
-        } */
         public bool IsPrime(int candidate)
         {
             if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+        public bool IsEven(int candidate)
+        {
+            if (candidate % 2 == 0)
             {
                 return false;
             }
